ae2996f9c7b77e11e921fd79587263f51e7b24a3
Vladyslav Terebylo <terebylov@ssemi.cz>
2025-07-29 16:07:32 +0200

    Created SafeRecruitment project. Set up docker-compose file and application.yaml file

---
+++ src/main/java/com/tvo/technologies/saferecruitment/SafeRecruitmentApplication.java
@@ -0,0 +1,13 @@
+package com.tvo.technologies.saferecruitment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeRecruitmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SafeRecruitmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tvo/technologies/saferecruitment/SafeRecruitmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tvo.technologies.saferecruitment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeRecruitmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
